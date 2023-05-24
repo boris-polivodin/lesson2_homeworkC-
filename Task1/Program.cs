62873e871a1800475174f6b37d79a857f39a574f
Boris <89241300089@mail.ru>
2023-05-24 12:42:23 +0900

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+class Program {
+    public static void Main (string[] args) {
+        int number = 0;
+        Console.Write("Input a three-digit number: ");
+        number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Вторая цифра: " + number/10%10);
+    }
+}
